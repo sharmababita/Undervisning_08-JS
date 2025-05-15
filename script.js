6b7f0b0ae4b4d9b38cb012eb7339ddc91c97839a
sharmababita <sharma.089babita@gmail.com>
2025-05-15 12:43:45 +0200

    JS function

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
+//console referer til inspiser vinduet. log gir en beskjed til console
+
 console.log('Hello!');
+
+// Datatyper:
+
+// string= streng/tekst
+
+// number= tall
+// bolean = true/false verdi sant/usant verdi
+
+// variabler:
+// let= kan bytte  verdi
+// const= ikke bytte verdi
+
+// Erklæer en variabel:
+//let ? = ?
+
+let userName ="Babita";
+console.log(userName);
+userName = "Julius";
+console.log(userName);
+
+let userAge= 32;
+console.log(userAge);
+userAge= 28;
+console.log(userAge);
+
+let userPresent= true;
+console.log(userPresent);
+
+userPresent= false;
+console.log(userPresent);
+
+
+const firstName= "Babita";
+const lastName= "Sharma";
+const fullName= firstName +  " " + lastName  + "!"; // variable chaining
+const wholeName = `${firstName} ${lastName}!`; // Template litteral
+console.log(fullName);
+
+function businessCard() {
+ const firstName="Babita";
+ const lastName= "Sharma";
+ console.log(lastName);
+}
+ businessCard()
+
+
+function myCard() {
+ const firstName = "Alex";
+    const lastName = "Siamon";
+    const fullName = `${firstName} ${lastName}!`;
+    console.log(fullName);
+
+    //Mer om dette på tirsdag
+    
+    const body = document.querySelector("h1");
+    body.textContent = fullName;
+}
+myCard()
+
+
+
+
+
+
+
+
+
